71463bd037be4979998ffb72f30931d112fa8356
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-10-31 04:13:15 +0600

    added some get and post api

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-// console.log(uri);
+
 // server and mongodb connection
 async function run() {
     try {
         await client.connect();
         const database = client.db("tourWorld");
         const destinationCollection = database.collection("destination");
+        const placeBookingCollection = database.collection("placeOrder");
         // get api for all data
         app.get("/allbooking", async (req, res) => {
             const cursor = destinationCollection.find({});
@@ -29,11 +30,45 @@ async function run() {
             res.send(destinations);
         });
 
+        //get api for a single data
+        app.get("/allbooking/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const singleBookingInfo = await destinationCollection.findOne(
+                query
+            );
+            // console.log(singleBookingInfo);
+            res.send(singleBookingInfo);
+        });
+
         // Post api
         app.post("/allbooking", async (req, res) => {
             const booking = req.body;
             const result = await destinationCollection.insertOne(booking);
             // console.log("A document was inserted with the _id:", result);
+            res.json(result);
+        });
+        // get api
+        app.get("/manageallorder", async (req, res) => {
+            const manageorder = await placeBookingCollection.find({}).toArray();
+            res.send(manageorder);
+        });
+        // get api for place booking
+        app.get("/mybooking/:email", async (req, res) => {
+            const email = req.params.email;
+            const mybooking = await placeBookingCollection
+                .find({ email })
+                .toArray();
+            // console.log(mybooking);
+            res.send(mybooking);
+        });
+
+        // post api for booking order collection
+
+        app.post("/placebooking", async (req, res) => {
+            const placebooking = req.body;
+            const result = await placeBookingCollection.insertOne(placebooking);
+            console.log("A document was inserted with the _id:", result);
             res.json(result);
         });
     } finally {
